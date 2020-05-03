dfbdf5d17fc4aa4bf38a2c0db086f61a1c49de03
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-05-03 09:29:31 -0300

    Estilizaçao da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-masculino/20/D16-2254-120/D16-2254-120_detalhe1.jpg?ts=1586198239?ims=280x280"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-masculino/20/D16-2254-120/D16-2254-120_detalhe1.jpg?ts=1586198239?ims=280x280"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-masculino/20/D16-2254-120/D16-2254-120_detalhe1.jpg?ts=1586198239?ims=280x280"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-masculino/20/D16-2254-120/D16-2254-120_detalhe1.jpg?ts=1586198239?ims=280x280"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-masculino/20/D16-2254-120/D16-2254-120_detalhe1.jpg?ts=1586198239?ims=280x280"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-masculino/20/D16-2254-120/D16-2254-120_detalhe1.jpg?ts=1586198239?ims=280x280"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
